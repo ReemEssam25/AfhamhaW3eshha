000a6dabefa214b4b0d2c18ac6469031bf3396bd
ReemEssam25 <reem1252001@hotmail.com>
2019-10-04 13:51:17 +0200

    start

---
+++ app/src/main/java/com/example/afhamhaw3eshha/Fragments/ArticleFragment.java
@@ -0,0 +1,84 @@
+package com.example.afhamhaw3eshha.Fragments;
+
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.afhamhaw3eshha.Adapter.ArticleAdapter;
+import com.example.afhamhaw3eshha.Model.Post;
+import com.example.afhamhaw3eshha.R;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class ArticleFragment extends Fragment {
+
+    private RecyclerView recyclerView;
+    private ArticleAdapter adapter;
+    List <Post> posts;
+
+    public ArticleFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View v = inflater.inflate(R.layout.fragment_article, container, false);
+
+        recyclerView = v.findViewById(R.id.article_RecycleView);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+
+        posts = new ArrayList<>();
+        readArticles();
+
+    return v;
+    }
+
+    private void readArticles(){
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference("Articles");
+
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                posts.clear();
+                Post post1 =new Post ("rrrrrrr" , "eeeeeeeee,lklkp[spkp" );
+                posts.add(post1);
+                for (DataSnapshot snapshot : dataSnapshot.getChildren()){
+                    Post post2 = snapshot.getValue(Post.class);
+
+                    assert post2!= null;
+                    posts.add(post2);
+                }
+                adapter = new ArticleAdapter(posts , getContext());
+                recyclerView.setAdapter(adapter);
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+}
---
+++ app/src/main/java/com/example/afhamhaw3eshha/Fragments/WriteFragment.java
@@ -0,0 +1,92 @@
+package com.example.afhamhaw3eshha.Fragments;
+
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+
+import com.example.afhamhaw3eshha.R;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+
+public class WriteFragment extends Fragment {
+
+    DatabaseReference reference;
+   // FirebaseUser firebaseUser;
+
+    EditText titleEditText, articleEditText, img;
+    Button addButton;
+    Spinner spinner;
+
+    public WriteFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View v =  inflater.inflate(R.layout.fragment_write, container, false);
+
+        addButton = v.findViewById(R.id.add_Button);
+        titleEditText = v.findViewById(R.id.title_EditText);
+        articleEditText = v.findViewById(R.id.article_EditText);
+        spinner = v.findViewById(R.id.spinner);
+
+        spinner = (Spinner)v.findViewById(R.id.spinner);
+        List<String> list = new ArrayList<String>();
+        list.add("list 1");
+        list.add("list 2");
+        list.add("list 3");
+        ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(getActivity(),
+                android.R.layout.simple_spinner_item, list);
+        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(dataAdapter);
+
+
+        addButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                addArticle(titleEditText.getText().toString(), articleEditText.getText().toString(), String.valueOf(spinner.getSelectedItem()));
+            }
+        });
+
+        return v;
+    }
+
+    private void addArticle(String title , String article /* , String img*/ , String category ){
+
+        DatabaseReference reference1 = FirebaseDatabase.getInstance().getReference();
+
+        HashMap <String , Object> hashMap = new HashMap<>();
+
+        hashMap.put("title", title);
+        hashMap.put("article", article);
+        hashMap.put("img", "default");
+        hashMap.put("likes" , "0");
+        hashMap.put("category", category);
+
+        reference1.child("Articles").push().setValue(hashMap);
+
+    }
+
+}
